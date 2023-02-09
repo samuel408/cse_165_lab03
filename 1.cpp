21588d49fedb0d60a4de2f3d34c5b36c1c90dc31
Samuel <samuel@Samuels-MacBook-Pro.local>
2023-02-09 12:03:32 -0800

    finished first part of lab

--- 1.cpp
+++ 1.cpp
@@ -2,8 +2,35 @@
 #include <vector>
 
 using namespace std;
+void findWords(vector<string> arr,string word){
+    string current;
+    for(int i = 0; i < arr.size() - 1;i++){
+       current = arr[i];
+       if(current[0] == word[0] ){
+           cout << current << endl;
+       }
+
+    }
+
+}
 
 int main (){
+    string word = "lol";
+    vector<string> arr;
+    int size = word.length();
+    cout << "type in words,  when done simply type in 1 letter " << endl;
+    while ( size != 1){
+
+        cin >> word;
+        arr.push_back(word);
+        size = word.length();
+
+    }
+
+    findWords(arr, word);
+
+    
+    
 
     return 0;
 }
